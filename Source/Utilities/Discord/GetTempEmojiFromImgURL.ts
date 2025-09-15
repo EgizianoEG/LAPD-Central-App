59b928329c85ad27857560e64a6222ec7b8ee2ab
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2025-09-15 12:05:55 +0300

    feat(jobs): add auto-deletion of expired temporary emojis
    - implement cron job to delete expired temporary emojis based on their expiration timestamp
    - log errors for failed deletions using a structured logging mechanism
    - configure job to run every 10 seconds with timezone and error-handling options
    
    feat(utilities): add utility to create temporary emojis from image URLs
    
    - create function to generate temporary emojis with an expiration timestamp in the name
    - provide options for custom emoji names and expiration durations
    - handle errors during emoji creation and ensure the client is ready before execution

---
+++ Source/Utilities/Discord/GetTempEmojiFromImgURL.ts
@@ -0,0 +1,53 @@
+import { ApplicationEmoji, Client as DiscordClient } from "discord.js";
+import { App as DiscordApp } from "@DiscordApp";
+import { milliseconds } from "date-fns";
+import AppLogger from "@Utilities/Classes/AppLogger.js";
+
+interface GetTempEmojiFromImgURLOptions {
+  /** The URL of the image to use for the emoji. */
+  ImgURL: string;
+
+  /** The duration in milliseconds until the emoji is considered "expired". This is appended to the emoji name as a timestamp. Default is 1 hour. */
+  Expires?: number;
+
+  /** The base name for the emoji; a maximum of 11 characters. Default is "unknown". */
+  EmojiName?: string;
+}
+
+/**
+ * Creates a temporary emoji in the application's guild from a given image URL.
+ * The emoji's name is suffixed with `_temp_` and an expiration timestamp,
+ * which can be used to identify and clean up temporary emojis later.
+ *
+ * @param Options - The options for creating the temporary emoji.
+ * @returns A promise that resolves with the created emoji object on success, or `null` if an error occurs during creation.
+ */
+export default async function GetTempEmojiFromImgURL({
+  ImgURL,
+  Expires = milliseconds({ hours: 1 }),
+  EmojiName = "unknown",
+}: GetTempEmojiFromImgURLOptions): Promise<ApplicationEmoji | null> {
+  await DelayUntilReady(DiscordApp);
+  try {
+    return await DiscordApp.application!.emojis.create({
+      attachment: ImgURL,
+      name: `${EmojiName.slice(0, 11)}_temp_${Date.now() + Expires}`,
+    });
+  } catch (Err: unknown) {
+    AppLogger.error({
+      message: "Failed to create temporary emoji from image URL",
+      label: "Utilities:Discord:GetTempEmojiFromImgURL",
+      error: Err as Error,
+      stack: (Err as Error)?.stack,
+    });
+
+    return null;
+  }
+}
+
+function DelayUntilReady(Client: DiscordClient): Promise<void> {
+  return new Promise<void>((resolve) => {
+    if (Client.isReady()) return resolve();
+    Client.once("ready", () => resolve());
+  });
+}
