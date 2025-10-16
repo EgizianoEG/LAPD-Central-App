5060256c90d51ae9737ad6393037dd2fac60c5e8
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2025-10-16 20:34:24 +0300

    refactor(Formatters): improve Dedent function to replace multiple spaces with a single space

--- Source/Utilities/Strings/Formatters.ts
+++ Source/Utilities/Strings/Formatters.ts
@@ -804,17 +804,17 @@ export function FormatSortRDInputNames(
  * Removes excess unwanted indentation and extra spaces from a given string potentially caused by escaping newlines in a multiline string.
  *
  * This function processes the input string to dedent it and then performs
- * additional cleanup by replacing:
+ * additional cleanup by replacing multiple spaces in specific contexts with a single space:
  * - Periods followed by two or more spaces with a single space after the period.
- * - Words separated by two or more spaces with a single space.
+ * - Any non-whitespace characters separated by two or more spaces with a single space.
  *
  * @param text - The input string to be dedented and cleaned.
  * @returns The processed string with reduced indentation and extra spaces removed.
  */
 export function Dedent(text: string): string {
   return _Dedent(text)
-    .replace(/\.[^\S\r\n]{2,}([`'"</>\w])/g, ". $1")
-    .replace(/([`'"</>\w])[^\S\r\n]{2,}([`'"</>\w])/g, "$1 $2");
+    .replaceAll(/\.[^\S\r\n]{2,}(\S)/g, ". $1")
+    .replaceAll(/(\S)[^\S\r\n]{2,}(\S)/g, "$1 $2");
 }
 
 /**
